f1ed03db1cfd469cc81961c4a6140e0cc38f7de3
Skwardlow <gorskovdmitry@gmail.com>
2019-08-21 12:48:26 +0700

    initial commit

---
+++ src/main/java/ru/eltex/project/simpleappointer/SimpleappointerApplication.java
@@ -0,0 +1,13 @@
+package ru.eltex.project.simpleappointer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleappointerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleappointerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/eltex/project/simpleappointer/SimpleappointerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.eltex.project.simpleappointer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleappointerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
