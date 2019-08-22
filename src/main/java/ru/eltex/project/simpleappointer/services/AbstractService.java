8b964ee9b4964f51fe8de279d32306c340cb777a
Skwardlow <gorskovdmitry@gmail.com>
2019-08-22 18:35:45 +0700

    Specialist service modified

--- src/main/java/ru/eltex/project/simpleappointer/services/AbstractService.java
+++ src/main/java/ru/eltex/project/simpleappointer/services/AbstractService.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.eltex.project.simpleappointer.dao.CommonRepository;
 import ru.eltex.project.simpleappointer.entities.Parent;
 
-public abstract class AbstractService<E extends Parent, R extends CommonRepository<E>> implements CommonService {
+public abstract class AbstractService<E extends Parent, R extends CommonRepository<E>>{
     protected final R repository;
 
-    //@Autowired
+
     public AbstractService(R repository){
         this.repository = repository;
     }
--- src/main/java/ru/eltex/project/simpleappointer/services/SpecialistService.java
+++ src/main/java/ru/eltex/project/simpleappointer/services/SpecialistService.java
@@ -4,10 +4,35 @@ import org.springframework.stereotype.Service;
 import ru.eltex.project.simpleappointer.dao.SpecialistRepository;
 import ru.eltex.project.simpleappointer.entities.Specialist;
 
+import java.util.List;
+
 @Service
 public class SpecialistService extends AbstractService<Specialist, SpecialistRepository> {
 
     public SpecialistService(SpecialistRepository repository){
         super(repository);
+    }
+
+    public void create(Specialist specialist){
+        repository.save(specialist);
+    }
+
+    public Specialist readById(Long id){
+        if (repository.findById(id).isPresent()){
+            return repository.findById(id).get();
+        }
+        return null;
+    }
+
+    public List<Specialist> readAll(){
+        new Lists.newArrayList(repository.findAll());
+    }
+
+    public void update(Specialist specialist){
+        repository.save(specialist);
+    }
+
+    public void delete(Specialist specialist){
+        repository.delete(specialist);
     }
 }
