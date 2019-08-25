29cda4b9db57c6960fc0e830c8b80eeb206a2eeb
vladimir <kiselev-v.s.99@yandex.ru>
2019-08-26 00:59:48 +0700

    Base Rest controller for registry specialist

--- src/main/java/ru/eltex/project/simpleappointer/controllers/MyRESTController.java
+++ src/main/java/ru/eltex/project/simpleappointer/controllers/MyRESTController.java
@@ -3,6 +3,7 @@ package ru.eltex.project.simpleappointer.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import ru.eltex.project.simpleappointer.entities.Specialist;
 import ru.eltex.project.simpleappointer.entities.User;
 import ru.eltex.project.simpleappointer.services.UserService;
 import ru.eltex.project.simpleappointer.utils.RegUtil;
@@ -33,6 +34,20 @@ public class MyRESTController {
         }
         User user = new User(req[0] + " " + req[1] + " " + req[2], req[3] , req[3] , req[4] , req[5] );
         byte answer = regUtil.regAddU(user);
+        return answer;
+    }
+
+    @RequestMapping(value = "/reg_spec",  produces = MediaType.APPLICATION_JSON_VALUE)
+    public Byte reg_spec(@RequestBody String json) throws UnsupportedEncodingException {
+        String[] req = URLDecoder.decode(json, "UTF-8").split("&");
+        for(int i = 0; i < req.length; i++){
+            int index = req[i].indexOf('=');
+            req[i] = req[i].substring(index + EXCLUDING_INDEX);
+            System.out.println(req[i]);
+        }
+        // Проверка инвайта req[5];
+        Specialist specialist = new Specialist( req[0] + " " + req[1] + " " + req[2], req[3] , req[3] , req[4] , req[6] );
+        byte answer = regUtil.regAddS(specialist);
         return answer;
     }
 }
