d0e30cbf8795516a8e64b3a05f7e712107843c8a
Skwardlow <gorskovdmitry@gmail.com>
2019-08-26 17:56:07 +0700

    added spring security dao,services...everything

--- src/main/java/ru/eltex/project/simpleappointer/services/UserDetailsServiceImpl.java
+++ src/main/java/ru/eltex/project/simpleappointer/services/UserDetailsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import ru.eltex.project.simpleappointer.dao.UserRepository;
 import ru.eltex.project.simpleappointer.entities.Role;
 import ru.eltex.project.simpleappointer.entities.User;
@@ -13,7 +14,7 @@ import ru.eltex.project.simpleappointer.entities.User;
 import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.Set;
-
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
