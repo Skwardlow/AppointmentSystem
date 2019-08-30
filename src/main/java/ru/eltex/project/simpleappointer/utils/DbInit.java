a3ac828927ef2617e557de3a79b5829dfd8da593
Skwardlow <gorskovdmitry@gmail.com>
2019-08-30 11:36:30 +0700

    started init adding for CMS templates preloading system

--- src/main/java/ru/eltex/project/simpleappointer/utils/DbInit.java
+++ src/main/java/ru/eltex/project/simpleappointer/utils/DbInit.java
@@ -7,7 +7,16 @@ import ru.eltex.project.simpleappointer.dao.UserRepository;
 import ru.eltex.project.simpleappointer.entities.Role;
 import ru.eltex.project.simpleappointer.entities.User;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Collections;
+import java.util.Properties;
+
 @Component
 public class DbInit implements CommandLineRunner {
     @Autowired
@@ -15,11 +24,26 @@ public class DbInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User("admin", "admin", "admin", "admin", "qwe12345");
+        InputStream input = new FileInputStream("src/main/resources/template.properties");
+        Properties prop = new Properties();
+        prop.load(input);
+
+
+        try(InputStream in = new URL(prop.getProperty("background-image")).openStream()){
+            Files.copy(in, Paths.get("src/main/resources/static/img.jpg"), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        User user = new User
+                ("admin", "admin", "admin", "admin", prop.getProperty("admin-password"));
         user.setRoles(Collections.singleton(Role.ADMIN));
         user.setActive(true);
         if (!userRepository.existsByUsername("admin")) {
             userRepository.save(user);
         }
+        else {
+            userRepository.deleteByUsername("admin");
+            userRepository.save(user);
+        }
+
     }
 }
